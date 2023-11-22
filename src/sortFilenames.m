8c3e480bc9f6ff5f1e2bb3ba83e617bde393b9fa
petoe <marton.petoe@ovgu.de>
2023-11-22 10:54:17 +0100

    Moved sorting of the list of filenames into a separate file that can be called by other functions as well.

--- src/sortFilenames.m
+++ src/sortFilenames.m
@@ -1,8 +1,4 @@
-function mergeParaviewFiles2Collection(sourceFolder,goalFolder,goalFilename,timesteps)
-
-
-f = struct2cell(dir(fullfile(sourceFolder,[goalFilename '*.vtu'])))';
-sourceFilenames = f(:,1);
+function filenames = sortFilenames(filenames,constStr)
 
 %% Adjust filenames internally
 % In some cases, the filenames in the 'sourceFilenames' do not contain step
@@ -22,10 +18,24 @@ sourceFilenames = f(:,1);
 % up by Matlab. Therefore, the step number are read and stored in the
 % variable 'val'. Then, 'val' is sorted and the sorting ids saved in 'idx'
 % is applied to sourceFilenames.
-sourceFilenames = sortFilenames( sourceFilenames, goalFilename );
 
+val = zeros(length(filenames),1);
+for i = 1:length(filenames)
+   s = split(filenames{i},{constStr,'.vtu'});
 
-write_pvd(sourceFolder,sourceFilenames,goalFolder,goalFilename,timesteps)
+   % Find non-empty cells:
+   ID = find( ~cellfun(@isempty,s) == 1 );
 
+   if isempty(ID)
+        error('The filename should be given in a format: "goalFilename######.vtu"')
+   end
+   ID = s{ID(end)};
+
+   ID = regexp(ID,'\d*','Match');
+   ID = ID{1};
+   val(i) = str2double(ID);
 end
 
+[~,idx] = sort(val);
+filenames = filenames(idx);
+end
